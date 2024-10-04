721db9a98203ef21de9835094417bc3bb07839fe
shantanu-ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2024-10-04 09:57:54 -0400

    New Files added for both courses

--- Robot Perception (ROB-GY 6203)/Homework/HW1/Q2/q2_ans.py
+++ Robot Perception (ROB-GY 6203)/Homework/HW1/Q2/q2_ans.py
@@ -1,4 +1,53 @@
-import cv2
+# Import PyTorch
+import torch
+from torch import nn
+
+# Import torchvision
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+
+# Check versions
+# Note: your PyTorch version shouldn't be lower than 1.10.0 and torchvision version shouldn't be lower than 0.11
+print(
+    f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
+
+
+# Setup device-agnostic code
+if torch.cuda.is_available():
+    device = "cuda"  # NVIDIA GPU
+elif torch.backends.mps.is_available():
+    device = "mps"  # Apple GPU
+else:
+    device = "cpu"  # Defaults to CPU if NVIDIA GPU/Apple GPU aren't available
+
+print(f"Using device: {device}")
+
+
+# Setup training data
+train_data = datasets.FashionMNIST(
+    root="data",  # where to download data to?
+    train=True,  # get training data
+    download=True,  # download data if it doesn't exist on disk
+    transform=ToTensor(),  # images come as PIL format, we want to turn into Torch tensors
+    target_transform=None  # you can transform labels as well
+)
+
+# Setup testing data
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,  # get test data
+    download=True,
+    transform=ToTensor()
+)
+
 
 if __name__ == "__main__":
+    # print("DEBUG POINT")
+    img, lbl = train_data[0]
+    plt.imshow(img.squeeze())
+    plt.waitforbuttonpress()
     pass
